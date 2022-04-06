4f85209c03a1dbac70a4cc77d2293ed43da8a59a
Roleeygit <juhaszroli2001@gmail.com>
2022-04-06 12:15:28 +0200

    Kezdés

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tama 0.0.1");
+        Tama tama = new Tama("Szamóca");
+        tama.ertelmezo();
+    }
+}
